91010bde63b306126db161d7eaef2545912f9bd2
Kambiz Saffari <k.saffarizadeh@gmail.com>
2023-11-30 13:24:56 -0600

    Create Appointment and CustomerAppointment classes.

---
+++ ServiceClassProject/Model/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public DateTime date { get; set; }
+
+}
---
+++ ServiceClassProject/Model/CustomerAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+public class CustomerAppointment
+{
+    public Customer customer { get; set; }
+    public Appointment appointment { get; set; }
+
+    public CustomerAppointment(Customer c, Appointment a)
+    {
+        customer = c;
+        appointment = a;
+    }
+}
